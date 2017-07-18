13c2b8ffe9f9f3c0fc156940b9f076436ebfbf3c
nory <norykloppackel@gmail.com>
2017-07-18 15:47:29 -0700

    Start creating fields in database

--- site360/models.py
+++ site360/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    #IN PROGRESS
+    username = models.CharField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
