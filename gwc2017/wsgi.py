2c85fc8bcbde2fd270d052e2bfe49c574364855e
nory <norykloppackel@gmail.com>
2017-07-18 15:31:57 -0700

    Fix errors referencing gwc2016 instead of gwc2017

--- gwc2017/wsgi.py
+++ gwc2017/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gwc2016 project.
+WSGI config for gwc2017 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gwc2016.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gwc2017.settings")
 
 application = get_wsgi_application()
