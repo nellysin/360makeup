01add7cd317c705ab86085e1b073605f75d1dc42
nory <norykloppackel@gmail.com>
2017-07-19 08:52:13 -0700

    Add empty main page

--- site360/models.py
+++ site360/models.py
@@ -4,15 +4,25 @@ from django.utils import timezone
 
 class User(models.Model):
     #IN PROGRESS
-    username = models.CharField()
+    username = models.CharField(max_length=20)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    test = ""
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.username
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    description = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
