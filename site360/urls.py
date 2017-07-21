0342e71d3e027ba54fe271a808625de3654a41d7
nory <norykloppackel@gmail.com>
2017-07-21 10:41:32 -0700

    Add login feature

--- site360/urls.py
+++ site360/urls.py
@@ -1,17 +1,26 @@
 from django.conf.urls import url
+from django.conf import settings
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^about-us/$', views.about_us, name='about_us'),
 
-    #Category search page regex explanation:
-    #First character must be a letter
-    #After that it can have lowercase letters, -s, and numbers
+    # Product detail page
+    # Regex explanation:
+    # First character must be a letter
+    # After that it can have lowercase letters, -s, and numbers
     url(r'^product/(?P<productname>([A-Z]|[a-z])([A-Z]|[a-z]|-|\d)*)/$', views.product_detail, name='product_detail'),
-    url(r'^signuplogin/$', views.signup_login, name='signup_login'),
+
+    # User-related pages
+    url(r'^login/$', auth_views.login, {'template_name': '/360/site360/templates/site360/signuplogin.html'}, name='signup_login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
     url(r'^profile/(?P<username>\w+)$', views.profile_info, name='profile_info'),
+
+    # Search-related pages
     url(r'^category/(?P<categoryname>([A-Z]|[a-z])*)/$', views.category_search, name='category_search'),
-    #Delete later
+
+    # Delete later
     url(r'testprofile/$', views.test_profile, name="test_profile")
 ]
--- site360/views.py
+++ site360/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
@@ -43,6 +44,14 @@ def about_us(request):
     return render(request, 'site360/about_us.html', {})
 
 def signup_login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+    else:
+        pass # Return an 'invalid login' error message.
     return render(request, 'site360/signuplogin.html', {})
 
 def profile_info(request, username):
