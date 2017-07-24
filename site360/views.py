c1c526e288a9e1cd3bba75ec4bbb755a961d917f
nory <norykloppackel@gmail.com>
2017-07-24 09:12:26 -0700

    What did I do

--- site360/views.py
+++ site360/views.py
@@ -98,10 +98,11 @@ def signup_login(request):
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(request, username=username, password=password)
+    print('ahs')
     if user is not None:
         login(request, user)
         # Redirect to a success page.
-        user_profile = Profile.objects.filter(user=user)
+        user_profile = Profile.objects.filter(user=user).all()
         if user_profile.count() == 0:
             profile = Profile()
             profile.user = user
